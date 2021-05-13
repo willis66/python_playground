be1519b4243f89f18a3f1bf6055f67f3c2a6bf68
Willis Erdman <willis.erdman@gmail.com>
2021-05-13 11:17:50 -0700

    more edits?

--- python_stockthing.py
+++ python_stockthing.py
@@ -1,8 +1,1 @@
-import requests
-
-def main():
-    url = "https://senate-stock-watcher-data.s3-us-west-2.amazonaws.com"
-    req = requests.get(url)
-    print(req.text)
-
-main()
+print("Hello World!")
