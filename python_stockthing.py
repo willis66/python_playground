345089c268dacef8a2ee399b35ca3e3171f4d812
Willis Erdman <willis.erdman@gmail.com>
2021-05-13 12:08:46 -0700

    downloading the csv file

--- python_stockthing.py
+++ python_stockthing.py
@@ -1,1 +1,8 @@
-print("Hello World!")
+import requests
+
+def main():
+    url = "https://senate-stock-watcher-data.s3-us-west-2.amazonaws.com"
+    req = requests.get(url)
+    print(req.text)
+
+main()
