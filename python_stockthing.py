453dde54230de5633fc3f918dc0b9bde9dfebc60
Willis Erdman <willis.erdman@gmail.com>
2021-05-13 12:09:10 -0700

    downloading the csv file

--- python_stockthing.py
+++ python_stockthing.py
@@ -1,8 +1,11 @@
 import requests
 
 def main():
-    url = "https://senate-stock-watcher-data.s3-us-west-2.amazonaws.com"
-    req = requests.get(url)
-    print(req.text)
+    url = "https://senate-stock-watcher-data.s3-us-west-2.amazonaws.com/aggregate/all_transactions.csv"
+    down_file = requests.get(url)
+    local_file = "agg_data.csv"
+
+    with open(local_file, 'wb')as file:
+        file.write(down_file.content)
 
 main()
