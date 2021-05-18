75ed254bb9dd87267d2fb7d166ca802f1dc7ac58
Willis Erdman <willis.erdman@gmail.com>
2021-05-18 14:28:25 -0700

    adding some data processing functionality

--- python_stockthing.py
+++ python_stockthing.py
@@ -1,11 +1,20 @@
 import requests
 
-def main():
+def downloader():
     url = "https://senate-stock-watcher-data.s3-us-west-2.amazonaws.com/aggregate/all_transactions.csv"
     down_file = requests.get(url)
     local_file = "agg_data.csv"
 
     with open(local_file, 'wb')as file:
         file.write(down_file.content)
+
+def parseer():
+    with open("agg_data.csv", "r") as file:
+        read_list = file.readlines()
+        print(read_list)
+
+def main():
+    downloader()
+    parseer()
 
 main()
