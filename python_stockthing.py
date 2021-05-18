da4fee54a55a931dd90abdafaf3c449ede8a7540
Willis Erdman <willis.erdman@gmail.com>
2021-05-18 15:34:30 -0700

    cleaning things up

--- python_stockthing.py
+++ python_stockthing.py
@@ -1,17 +1,18 @@
 import requests
 
+local_file = "agg_data.csv"
+
 def downloader():
     url = "https://senate-stock-watcher-data.s3-us-west-2.amazonaws.com/aggregate/all_transactions.csv"
     down_file = requests.get(url)
-    local_file = "agg_data.csv"
 
     with open(local_file, 'wb')as file:
         file.write(down_file.content)
 
 def parseer():
-    with open("agg_data.csv", "r") as file:
+    with open(local_file, "r") as file:
         read_list = file.readlines()
-        print(read_list)
+        #print(read_list)
 
 def main():
     downloader()
